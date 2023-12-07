efc15174e190f22cdd1f03cfe76046118e1cdae8
Helwor <helwor@yahoo.com>
2023-12-07 11:22:17 +0100

    Add files via upload

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name = 'Relentless Chicken',
+	description = 'continuous waves of chicken',
+	shortname = 'relchicken',
+	version = 'dev2',
+	mutator = '1',
+	game = '',
+	shortGame = '',
+	modtype = 1,
+	depend = {
+		[[rapid://zk:stable]]
+	},
+}
